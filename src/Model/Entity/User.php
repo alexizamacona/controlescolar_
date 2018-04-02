b7cedbd1d054cf7faee508003ac8bf8fd1165b51
Oscar <oolivares@converto.com.mx>
2018-04-02 10:08:39 -0500

    password hash
    tururu

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -56,4 +57,12 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    protected function _setPassword($password)
+    {
+        if(strlen($password)>0)
+        {
+            return (new DefaultPasswordHasher)->hash($password);
+        }
+    }
 }
