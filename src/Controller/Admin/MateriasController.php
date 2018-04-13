f0df85b8e99286bfecbdfaae293b3f014c512537
Jorgegaraz <37445676+Jorgegaraz@users.noreply.github.com>
2018-04-13 08:29:03 -0500

    admin/materias

--- src/Controller/Admin/MateriasController.php
+++ src/Controller/Admin/MateriasController.php
@@ -1,23 +1,34 @@
 <?php
-    namespace App\Controller\Admin;
+namespace App\Controller\Admin;
 
-    use App\Controller\AppController;
+use App\Controller\AppController;
 
-    class MateriasController extends AppController
-    {
-		public function add() {
-		 $materia = $this->Materias->newEntity();
-        	if ($this->request->is('post')) {
-            $materia = $this->Materias->patchEntity($materia, $this->request->getData());
-            if ($this->Materias->save($materia)) {
-                $this->Flash->success(__('The materia has been saved.'));
+class MateriasController extends AppController
+{
+	public function add() {
+		$materia = $this->Materias->newEntity();
+		if ($this->request->is('post')) {
+			$materia = $this->Materias->patchEntity($materia, $this->request->getData());
+			if ($this->Materias->save($materia)) {
+				$this->Flash->success(__('The materia has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The materia could not be saved. Please, try again.'));
-        	}
-        	$carreras = $this->Materias->Carreras->find('list');
-        	$this->set(compact('materia', 'carreras'));
-    	 }		
-    };
-  
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('The materia could not be saved. Please, try again.'));
+		}
+		$carreras = $this->Materias->Carreras->find('list');
+		$this->set(compact('materia', 'carreras'));
+	}		
+
+
+
+	public function index()
+	{
+		$materias = ($this->Materias->find());
+
+		$this->set(compact('materias'));
+	}
+
+
+
+};  
