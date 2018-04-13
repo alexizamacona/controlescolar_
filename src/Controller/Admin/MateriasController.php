cc2a7736241833b26513753b916b64d66c37ede6
Sergio Tafoya <sergio.tafoya@gmail.com>
2018-04-13 10:48:57 -0500

    /admin/materias/index

--- src/Controller/Admin/MateriasController.php
+++ src/Controller/Admin/MateriasController.php
@@ -1,13 +1,16 @@
 <?php
-    namespace App\Controller\Admin;
+namespace App\Controller\Admin;
 
-    use App\Controller\AppController;
+use App\Controller\AppController;
 
-    class MateriasController extends AppController
-    {
-		public function add() {
-		 $materia = $this->Materias->newEntity();
-        	if ($this->request->is('post')) {
+class MateriasController extends AppController
+{
+
+    /********************************************************/
+
+    public function add() {
+        $materia = $this->Materias->newEntity();
+        if ($this->request->is('post')) {
             $materia = $this->Materias->patchEntity($materia, $this->request->getData());
             if ($this->Materias->save($materia)) {
                 $this->Flash->success(__('The materia has been saved.'));
@@ -15,9 +18,21 @@
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The materia could not be saved. Please, try again.'));
-        	}
-        	$carreras = $this->Materias->Carreras->find('list');
-        	$this->set(compact('materia', 'carreras'));
-    	 }		
-    };
-  
+        }
+        $carreras = $this->Materias->Carreras->find('list');
+        $this->set(compact('materia', 'carreras'));
+    }
+
+    /********************************************************/
+
+    public function index(){
+        $materias = $this->Materias->find()
+            ->contain(['Carreras'])
+            ->order(['Carreras.name','Materias.grado','Materias.name']);
+
+        $this->set(compact("materias"));
+    }
+
+    /********************************************************/
+
+};
