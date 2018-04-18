30b085029885e60ee4ab89cda7ebd63c142a1fba
mares93 <37445950+mares93@users.noreply.github.com>
2018-04-18 07:43:44 -0500

    permitir login,logout y registrar

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,6 +45,8 @@ class AppController extends Controller
         $this->loadComponent('Flash');
         $this->LoadComponent('Auth');
 
+        $this->Auth->allow(['login','logout','registrar']);
+
         /*
          * Enable the following components for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
