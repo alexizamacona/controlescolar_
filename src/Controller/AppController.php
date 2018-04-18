431a179eb55507df940e9f8b223dee5dea7bed06
alexizamacona <37445696+alexizamacona@users.noreply.github.com>
2018-04-18 07:30:08 -0500

    login

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,6 +43,7 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->LoadComponent('Auth');
 
         /*
          * Enable the following components for recommended CakePHP security settings.
---
+++ src/Template/Users/login.ctp
@@ -0,0 +1,7 @@
+<h2>Login</h2>
+
+<?=$this->Form->create()?>
+<?=$this->Form->control("username")?>
+<?=$this->Form->control("password")?>
+<?=$this->Form->button("Entrar")?>
+<?=$this->Form->end()?>
