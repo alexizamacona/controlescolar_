a5b5c31ea388ee4622026002c1a8a21fca44f143
mares93 <37445950+mares93@users.noreply.github.com>
2018-04-25 12:40:59 -0500

    Admin/grupos/edit

--- src/Controller/Admin/GruposController.php
+++ src/Controller/Admin/GruposController.php
@@ -31,15 +31,38 @@ class GruposController extends AppController
   public function view($id=null)
   {
     $grupo = $this->Grupos->get($id, [
-            'contain' => ['Users', 'Materias']
-        ]);
+      'contain' => ['Users', 'Materias']
+    ]);
     $this->set('grupo', $grupo);
   }
 
   public function index(){
     $grupos = $this->Grupos->find()->contain(['Materias','Users']);
-        
-        
-        $this->set(compact('grupos'));
+
+
+    $this->set(compact('grupos'));
+  }
+
+  public function edit($id = null){
+    $grupo= $this->Grupos->get($id, [
+      'contain' => []
+    ]);
+    if ($this->request->is(['patch', 'post', 'put'])) {
+      $grupo = $this->Grupos->patchEntity($grupo, $this->request->getData());
+      if ($this->Grupos->save($grupo)) {
+        $this->Flash->success(__('El grupo ha sido modificado exitosamente.'));
+
+        return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('El grupo no ha podido ser modificado. Por favor intenta de nuevo.'));
+    }
+    $users    = $this->Grupos->Users->find('list')->where(['Users.rol'=>'profesor']);
+    $materias = $this->Grupos->Materias->find('list');
+
+    $this->set(compact('grupo'));
+    $this->set(compact('materias'));
+    $this->set(compact('users'));
+
+    
   }
 }
