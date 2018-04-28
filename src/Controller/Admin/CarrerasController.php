892d3c5c309e9b0fec95c4ec195dc89998350638
vi1cko0 <victor_ramirez31397@hotmail.com>
2018-04-28 15:11:48 -0500

    admin/carrerascontroller

--- src/Controller/Admin/CarrerasController.php
+++ src/Controller/Admin/CarrerasController.php
@@ -27,43 +27,7 @@ class CarrerasController extends AppController
         $this->set(compact("carrera"));
         $this->set(compact("periodos"));
     }
-    
-    public function view($id=null)
-    {
-        $carrera = $this->Carreras->get($id,['contain'=>['Periodos']]);
-        $this->set('carrera', $carrera);
-    }
-    
-    public function edit($id = null)
-    {
-        $carrera = $this->Carreras->get($id);
-        $periodos = $this->Carreras->Periodos->find("list");
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $carrera = $this->Carreras->patchEntity($carrera, $this->request->getData());
-            if ($this->Carreras->save($carrera)) {
-                $this->Flash->success(__('Carrera actualizada'));
-=======
-	public function index(){
-		$carreras = $this->Carreras->find()->contain(['Periodos']);       
-		$this->set(compact('carreras'));
-	}
-/****************************************************/
-	public function add()
-	{
-		$periodos = $this->Carreras->Periodos->find("list");
-		$carrera = $this->Carreras->newEntity();
-		if ($this->request->is('post')) {
-			$carrera = $this->Carreras->patchEntity($carrera, $this->request->getData());
-			if ($this->Carreras->save($carrera)) {
-				$this->Flash->success(__('La carrera ha sido guardada'));
 
-				return $this->redirect(['action' => 'index']);
-			}
-			$this->Flash->error(__('Problemas al guardar la carrera, intentelo nuevamente.'));
-		}
-		$this->set(compact("carrera"));
-		$this->set(compact("periodos"));
-	}
 /****************************************************/
 	public function view($id=null)
 	{
@@ -82,7 +46,6 @@ class CarrerasController extends AppController
 			$carrera = $this->Carreras->patchEntity($carrera, $this->request->getData());
 			if ($this->Carreras->save($carrera)) {
 				$this->Flash->success(__('Carrera actualizada'));
->>>>>>> dd45872fca2effbf8da96d7a28911c8ce00eeb12
 
 				return $this->redirect(['action' => 'index']);
 			}
